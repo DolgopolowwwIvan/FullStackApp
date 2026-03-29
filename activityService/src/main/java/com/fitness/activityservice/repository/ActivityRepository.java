29337650394e9aa6ea6361651c9f488a2168e4ab
Ivan Dolgopolov <fertexop@yandex.ru>
2026-03-29 16:03:04 +0700

    feat(Activity): Закончил базовый функционал ActivityService и начал на работу c Eureka Server

--- activityService/src/main/java/com/fitness/activityservice/repository/ActivityRepository.java
+++ activityService/src/main/java/com/fitness/activityservice/repository/ActivityRepository.java
@@ -4,6 +4,16 @@ import com.fitness.activityservice.model.Activity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActivityRepository extends MongoRepository<Activity, String> {
+    // Spring Data использует процессор аннотаций
+    // и создает proxy-класс во время выполнения
+    // Создает запрос: { "userId" : userId }
+    // Выполняет: db.activities.find({ "userId" : userId })
+    // Возвращает список Activity
+    // Это работает если правильрно указывать имя метода
+    // findBy<Поле>
+    List<Activity> findByUserId(String userId);
 }
--- activityService/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityService/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -7,6 +7,9 @@ import com.fitness.activityservice.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ActivityService {
@@ -36,5 +39,23 @@ public class ActivityService {
         response.setCreatedAt(activity.getCreatedAt());
         response.setUpdatedAt(activity.getUpdatedAt());
         return response;
+    }
+
+    // Метод, который взаимодействует с базой данных и возвращает список активностей
+    public List<ActivityResponse> getUserActivities(String userId) {
+        // Так как в MongoDB к id обратиться проблематично,
+        // делаем фильт по полю userId
+        List<Activity> activities = activityRepository.findByUserId(userId);
+        // Нужно взять список, создать из него поток и замапить
+        // каждый элемент списка в ActivityResponse
+        return activities.stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ActivityResponse getActivityById(String activityId) {
+        return activityRepository.findById(activityId)
+                .map(this::mapToResponse)
+                .orElseThrow(() -> new RuntimeException("Activity with id: " + activityId + " not found!"));
     }
 }
